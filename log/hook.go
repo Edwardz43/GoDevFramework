2805e60eeaa08e5c215043d4e7cf15e554e84811
EdLo <b9330315@gmail.com>
2020-04-16 21:17:08 +0800

    [ADD] Hooks elasticsearch to logger

---
+++ log/hook.go
@@ -0,0 +1,65 @@
+package log
+
+import (
+	"Edwardz43/godevframework/config"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	index string
+	url   string
+)
+
+func init() {
+	index = config.Log().HookIndex
+	url = fmt.Sprintf("%s/%s/_doc", config.Log().HookURL, index)
+}
+
+// Emit emits a log to elasticsearch host
+func Emit(c *Content) error {
+
+	reqBodyBytes := new(bytes.Buffer)
+
+	json.NewEncoder(reqBodyBytes).Encode(c)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBodyBytes.Bytes()))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("send ES log err : %v\n", err)
+	}
+
+	if resp == nil {
+		return fmt.Errorf("send message no response")
+	}
+
+	b := resp.Body
+
+	msg, err := ioutil.ReadAll(b)
+	if err != nil {
+		return err
+	}
+
+	log.Println(string(msg))
+
+	return nil
+}
+
+// Content is a log content data model
+type Content struct {
+	Level   string    `json:"level"`
+	Date    time.Time `json:"date"`
+	Caller  string    `json:"caller"`
+	Message string    `json:"message"`
+	Stack   string    `json:"stack"`
+}
---
+++ log/hook_test.go
@@ -0,0 +1,20 @@
+package log_test
+
+import (
+	"Edwardz43/godevframework/log"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmitThenReturnSuccess(t *testing.T) {
+	c := &log.Content{
+		Level:   "Info",
+		Message: "Test ES Log",
+		Date:    time.Now(),
+		Caller:  "zaplogger/zaplogger.go:104",
+	}
+	err := log.Emit(c)
+	assert.Nil(t, err)
+}
